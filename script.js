789b4083fa526f6d9cd162014a1b3eecfd4ccc79
trongvinhne <309741773+trongvinhne@users.noreply.github.com>
2026-07-28 00:54:52 +0700

    h

--- script.js
+++ script.js
@@ -1,14 +1,65 @@
-const videoInput = document.getElementById("videoInput");
 const video = document.getElementById("video");
+const input = document.getElementById("videoInput");
+const button = document.getElementById("scanButton");
+const status = document.getElementById("status");
+const result = document.getElementById("result");
 
-videoInput.addEventListener("change", function () {
+input.onchange = () => {
+    const file = input.files[0];
+    if (!file) return;
 
-const file = this.files[0];
+    video.src = URL.createObjectURL(file);
+};
 
-if(!file) return;
+button.onclick = async () => {
 
-video.src = URL.createObjectURL(file);
+    if (!video.src) {
+        alert("Hãy chọn video");
+        return;
+    }
 
-video.style.display = "block";
+    await video.play();
+    video.pause();
 
-});
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    let frames = [];
+
+    const step = 0.5;
+
+    for (let t = 0; t < video.duration; t += step) {
+
+        video.currentTime = t;
+
+        await new Promise(resolve => {
+
+            video.onseeked = resolve;
+
+        });
+
+        ctx.drawImage(video,0,0);
+
+        frames.push(canvas.toDataURL("image/jpeg"));
+
+        status.innerHTML =
+        "Đang đọc video: " +
+        Math.round(t/video.duration*100)
+        +"%";
+
+    }
+
+    status.innerHTML =
+    "Đã lấy "
+    +frames.length+
+    " khung hình";
+
+    result.innerHTML =
+    "<b>Sẵn sàng OCR...</b>";
+
+    console.log(frames);
+
+};
